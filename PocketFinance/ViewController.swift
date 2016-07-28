7c4a3bc337afa1f97eec3af1c9409e0e7c8e67a7
Weirui Kong <weiruikong@cs-auth-dc-129-97-60-235.dynamic.uwaterloo.ca>
2016-07-28 11:27:31 -0400

    Initial Commit

---
+++ PocketFinance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PocketFinance
+//
+//  Created by Weirui Kong on 2016-07-28.
+//  Copyright © 2016 uwaterloo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
